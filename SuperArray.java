2fe792a4be3bba23b0603a930e077f42b67ca5c5
Derek <dyin00@stuy.edu>
2018-10-24 22:57:45 -0400

    commit

--- SuperArray.java
+++ SuperArray.java
@@ -1,10 +1,24 @@
 public class SuperArray {
-  private int Index;
-  private double value;
-  private String value;
-  private char value;
-  
-  public SuperArray(){
+  private String[] data;
+  private int size;
+  public SuperArray(String[] data1) {
+    data = data1;
   }
-
+  public int size() {
+    int c = 0;
+    for (int i = 0; i < data.length-1; i++){
+      if (!(data[i].equals(null))){
+        c++;
+      }
+    }
+    return c;
+  }
+  public String set(int index, String element){
+    String x = data[index];
+    data[index] = element;
+    return x;
+  }
+public void add(int index, String element){
+  
+}
 }
